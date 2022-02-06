4e854143a35fdd11a11590fda410f497caf3405e
AB <andrewbrowne2@gmail.com>
2022-02-06 15:28:21 +0000

    fix

--- controllers/endpoints.controllers.js
+++ controllers/endpoints.controllers.js
@@ -2,7 +2,7 @@ const fetchEndpoints = require('../models/endpoints.models')
 
 exports.getEndpoints = (req, res, next) => {
     fetchEndpoints().then((result) => {
-        res.status(200).send({ result })
+        res.status(200).send({ endpoints: result })
     })
     .catch((err) => {
         next(err)
